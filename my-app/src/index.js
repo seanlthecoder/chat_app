921fdf72bf7cfb2ef9585ff0f558d63077c281ee
Sean Lester Durham <sean.durham1982@gmail.com>
2023-05-14 18:07:07 -0500

    updated both client and server side

--- my-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// everything we create inside of react will be inside of this app
-import App from './app';
-
-// and we will put it inside of this root
-ReactDOM.render(<App />, document.querySelector('#root'));
-
