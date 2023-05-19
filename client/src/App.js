1d912fba5044468b461489035dccdf7f58403206
Sean Lester Durham <sean.durham1982@gmail.com>
2023-05-19 18:04:50 -0500

    updated app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,29 +3,35 @@ import './App.css';
 //calling io to establish a connection
 import io from 'socket.io-client'
 
+import { useState } from "react";
+
 // connecting the front end to the back end
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("http://localhost:3000");
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
 
   const joinRoom = () => {
-
+    // username is not empty and if room is not empty
+    if (username !=="" && room !=="") {
+      // this will recieve the function as data from the index.js file in order to join the room
+      socket.emit("join_room", room);
   }
-
+};
 
 
   return <div className="App">
     
     <h3> Join A Chat </h3>
-    <input type="text" placeholder="Sean...." onChange={(event) => {setUsername(event.target.value);
+    
+    <input type="text" placeholder="Sean...." onChange={(event) => {setUsername(event.target.value); // target.value is accessing the value of the input field
     }} 
     />
     <input type="text" placeholder="Room ID...." onChange={(event) => {setRoom(event.target.value);
     }}
        />
-    <button>Join A Room</button>
+    <button onClick={joinRoom}>Join A Room</button>
   
     </div>
   
